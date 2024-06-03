7a6abc1b30d8f9c3d458b99247475d9006a70a5e
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-06-03 20:32:41 +0100

    First build

--- Target.cs
+++ Target.cs
@@ -1,11 +1,16 @@
 using Godot;
 using System;
+using FireFight;
+using FireFight.CharacterObjects;
 
 public partial class Target : Node
 {
 	// Called when the node enters the scene tree for the first time.
+	private Character Character { get; set; }
+
 	public override void _Ready()
 	{
+		Character = new Character(7, 1);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- player.cs
+++ player.cs
@@ -1,11 +1,15 @@
+using FireFight.CharacterObjects;
 using Godot;
 using System;
 
 public partial class player : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
+	private Character Character { get; set; }
+
 	public override void _Ready()
 	{
+		Character = new Character(7, 0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
