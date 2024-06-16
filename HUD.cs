cfa999481fca64cf3fe8cbc475b0310d132fe6ff
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-06-16 14:41:45 +0100

    hud buttons connected

--- HUD.cs
+++ HUD.cs
@@ -1,18 +1,81 @@
 using Godot;
 using System;
 
-public partial class HUD : CanvasLayer
+public partial class hud : CanvasLayer
 {
-    [Signal]
-    public delegate void StartGameEventHandler();
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        System.Diagnostics.Debug.Print("fasffafzsdccddcs");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+    }
+
+    private void _on_n_pressed()
+    {
+        System.Diagnostics.Debug.Print("n");
+    }
+
+    private void _on_nw_pressed()
+    {
+        System.Diagnostics.Debug.Print("nw");
+    }
+
+    private void _on_ne_pressed()
+    {
+        System.Diagnostics.Debug.Print("ne");
+    }
+
+    private void _on_e_pressed()
+    {
+        System.Diagnostics.Debug.Print("e");
+    }
+
+    private void _on_se_pressed()
+    {
+        System.Diagnostics.Debug.Print("se");
+    }
+
+    private void _on_s_pressed()
+    {
+        System.Diagnostics.Debug.Print("s");
+    }
+
+    private void _on_sw_pressed()
+    {
+        System.Diagnostics.Debug.Print("sw");
+    }
+
+    private void _on_w_pressed()
+    {
+        System.Diagnostics.Debug.Print("w");
+    }
+
+    private void _on_fire_pressed()
+    {
+        System.Diagnostics.Debug.Print("fire");
+    }
+
+    private void _on_reload_pressed()
+    {
+        System.Diagnostics.Debug.Print("reload");
+    }
+
+    private void _on_target_pressed()
+    {
+        System.Diagnostics.Debug.Print("target");
+    }
+
+    private void _on_aim_pressed()
+    {
+        System.Diagnostics.Debug.Print("aim");
+    }
+
+    private void _on_end_turn_pressed()
+    {
+        System.Diagnostics.Debug.Print("end turn");
     }
 }
--- player.cs
+++ player.cs
@@ -8,31 +8,31 @@ using System.Reflection;
 
 public partial class player : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public Character Character { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public Character Character { get; set; }
 
-    public override void _Ready()
-    {
-        Character = new Character(7, 0);
-        Character.Xpos = (uint)Position.X;
-        Character.Ypos = (uint)Position.Y;
-        Character.CurrentTarget = null;
-        Character.MapScale = 100;
-        Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
-        Character.RangedWeapons[0].Equipped = true;
-        Character.CurrentAimAmount = 20;
+	public override void _Ready()
+	{
+		Character = new Character(7, 0);
+		Character.Xpos = (uint)Position.X;
+		Character.Ypos = (uint)Position.Y;
+		Character.CurrentTarget = null;
+		Character.MapScale = 100;
+		Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
+		Character.RangedWeapons[0].Equipped = true;
+		Character.CurrentAimAmount = 20;
 
-        //
+		//
 
-        //Character.FireFunction();
-    }
+		//Character.FireFunction();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        //Node2D targetNode = GetNode<Node2D>("/root/Main/Target");
-        //Target targetObject = (Target)targetNode;
-        //Character.CurrentTarget = targetObject.Character;
-        //Character.FireFunction();
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Node2D targetNode = GetNode<Node2D>("/root/Main/Target");
+		//Target targetObject = (Target)targetNode;
+		//Character.CurrentTarget = targetObject.Character;
+		//Character.FireFunction();
+	}
 }
