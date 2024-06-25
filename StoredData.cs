39f18417e54694e474843cd33ba44a3bc661e16d
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-06-25 14:51:53 +0100

    Spawning 4 soldiers

---
+++ StoredData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireFightGodot
+{
+    public static class StoredData
+    {
+        public static List<Soldier> Soldiers = new List<Soldier>();
+        public static uint NumberOfSoldiers = 2;
+    }
+}
