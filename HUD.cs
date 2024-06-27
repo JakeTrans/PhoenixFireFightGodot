91c04fa5788b79070e1a1cba05afd64efcc8d46a
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-06-27 10:50:21 +0100

    Reorganisation into folders

--- HUD.cs
+++ HUD.cs
@@ -4,98 +4,98 @@ using System;
 
 public partial class hud : CanvasLayer
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        //CurrentSoldierNode = GetNode<player>("/root/Main/Player");
-        //CurrentSoldierNode = StoredData.Soldiers[0];
-        //CurrentSoldierNode = StoredData.Soldiers[0];
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//CurrentSoldierNode = GetNode<player>("/root/Main/Player");
+		//CurrentSoldierNode = StoredData.Soldiers[0];
+		//CurrentSoldierNode = StoredData.Soldiers[0];
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 
-    private void _on_n_pressed()
-    {
-        System.Diagnostics.Debug.Print("n");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveN);
-    }
+	private void _on_n_pressed()
+	{
+		System.Diagnostics.Debug.Print("n");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveN);
+	}
 
-    private void _on_nw_pressed()
-    {
-        System.Diagnostics.Debug.Print("nw");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveNW);
-    }
+	private void _on_nw_pressed()
+	{
+		System.Diagnostics.Debug.Print("nw");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveNW);
+	}
 
-    private void _on_ne_pressed()
-    {
-        System.Diagnostics.Debug.Print("ne");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveNE);
-    }
+	private void _on_ne_pressed()
+	{
+		System.Diagnostics.Debug.Print("ne");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveNE);
+	}
 
-    private void _on_e_pressed()
-    {
-        System.Diagnostics.Debug.Print("e");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveE);
-    }
+	private void _on_e_pressed()
+	{
+		System.Diagnostics.Debug.Print("e");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveE);
+	}
 
-    private void _on_se_pressed()
-    {
-        System.Diagnostics.Debug.Print("se");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveSE);
-    }
+	private void _on_se_pressed()
+	{
+		System.Diagnostics.Debug.Print("se");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveSE);
+	}
 
-    private void _on_s_pressed()
-    {
-        System.Diagnostics.Debug.Print("s");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveS);
-    }
+	private void _on_s_pressed()
+	{
+		System.Diagnostics.Debug.Print("s");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveS);
+	}
 
-    private void _on_sw_pressed()
-    {
-        System.Diagnostics.Debug.Print("sw");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveSW);
-    }
+	private void _on_sw_pressed()
+	{
+		System.Diagnostics.Debug.Print("sw");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveSW);
+	}
 
-    private void _on_w_pressed()
-    {
-        System.Diagnostics.Debug.Print("w");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveW);
-    }
+	private void _on_w_pressed()
+	{
+		System.Diagnostics.Debug.Print("w");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.MoveW);
+	}
 
-    private void _on_fire_pressed()
-    {
-        System.Diagnostics.Debug.Print("fire");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.FireSingle);
-    }
+	private void _on_fire_pressed()
+	{
+		System.Diagnostics.Debug.Print("fire");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.FireSingle);
+	}
 
-    private void _on_reload_pressed()
-    {
-        System.Diagnostics.Debug.Print("reload");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.Reload);
-    }
+	private void _on_reload_pressed()
+	{
+		System.Diagnostics.Debug.Print("reload");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.Reload);
+	}
 
-    private void _on_target_pressed()
-    {
-        System.Diagnostics.Debug.Print("target");
-        //PlayerNode.Character.CurrentTarget = GetNode<player>("/root/Main/Target").Character;
-        //CurrentSoldierNode.Character.CurrentTarget = GetNode<Target>("/root/Main/Target").Character;
-    }
+	private void _on_target_pressed()
+	{
+		System.Diagnostics.Debug.Print("target");
+		//PlayerNode.Character.CurrentTarget = GetNode<player>("/root/Main/Target").Character;
+		//CurrentSoldierNode.Character.CurrentTarget = GetNode<Target>("/root/Main/Target").Character;
+	}
 
-    private void _on_aim_pressed()
-    {
-        System.Diagnostics.Debug.Print("aim");
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.Aim);
-    }
+	private void _on_aim_pressed()
+	{
+		System.Diagnostics.Debug.Print("aim");
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Add(FireFight.Classes.ActionsPossible.Aim);
+	}
 
-    private void _on_end_turn_pressed()
-    {
-        StoredData.CurrentSoldierNode.Character.DoAllActions();
-        System.Diagnostics.Debug.Print("end turn ran");
+	private void _on_end_turn_pressed()
+	{
+		StoredData.CurrentSoldierNode.Character.DoAllActions();
+		System.Diagnostics.Debug.Print("end turn ran");
 
-        StoredData.CurrentSoldierNode.GlobalPosition = new Vector2(StoredData.CurrentSoldierNode.Character.Xpos, StoredData.CurrentSoldierNode.Character.Ypos);
-        StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Clear();
-    }
+		StoredData.CurrentSoldierNode.GlobalPosition = new Vector2(StoredData.CurrentSoldierNode.Character.Xpos, StoredData.CurrentSoldierNode.Character.Ypos);
+		StoredData.CurrentSoldierNode.Character.ActionsForTurn.ActionsTaken.Clear();
+	}
 }
--- main.cs
+++ main.cs
@@ -8,34 +8,34 @@ using static System.Formats.Asn1.AsnWriter;
 
 public partial class main : Node
 {
-    private PackedScene Soldier = (PackedScene)GD.Load("res://Soldier.tscn");
+    private PackedScene Soldier = (PackedScene)GD.Load("res://Units/Soldier.tscn");
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         /*
-        Node Soldier1 = Soldier.Instantiate();
+		Node Soldier1 = Soldier.Instantiate();
 
-        AddChild(Soldier1);
+		AddChild(Soldier1);
 
-        Soldier a = (Soldier)Soldier1;
-        StoredData.Soldiers.Add(a);
-        a.Character.Xpos = 200;
+		Soldier a = (Soldier)Soldier1;
+		StoredData.Soldiers.Add(a);
+		a.Character.Xpos = 200;
 
-        a.Character.Ypos = 300;
-        a.GlobalPosition = new Vector2(a.Character.Xpos, a.Character.Ypos);
+		a.Character.Ypos = 300;
+		a.GlobalPosition = new Vector2(a.Character.Xpos, a.Character.Ypos);
 
-        Node Soldier2 = Soldier.Instantiate();
+		Node Soldier2 = Soldier.Instantiate();
 
-        AddChild(Soldier2);
+		AddChild(Soldier2);
 
-        Soldier b = (Soldier)Soldier2;
-        StoredData.Soldiers.Add(b);
-        b.Character.Xpos = 300;
+		Soldier b = (Soldier)Soldier2;
+		StoredData.Soldiers.Add(b);
+		b.Character.Xpos = 300;
 
-        b.Character.Ypos = 400;
-        b.GlobalPosition = new Vector2(b.Character.Xpos, b.Character.Ypos);
-        */
+		b.Character.Ypos = 400;
+		b.GlobalPosition = new Vector2(b.Character.Xpos, b.Character.Ypos);
+		*/
 
         for (uint i = 1; i < 5; i++)
         {
