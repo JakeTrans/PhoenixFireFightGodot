63a4f789aa8110d0c11e4c32842dc23acb01b0f8
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-07-14 17:56:56 +0100

    Animations added

--- Units/Soldier.cs
+++ Units/Soldier.cs
@@ -6,33 +6,33 @@ using System;
 
 public partial class Soldier : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public Character Character { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public Character Character { get; set; }
 
-    private AnimatedSprite2D _animatedSprite;
+	private AnimatedSprite2D _animatedSprite;
 
-    public override void _Ready()
-    {
-        Random rnd = new Random();
-        Character = new Character(7, 0);
-        Character.Name = rnd.Next(1, 10000).ToString();
-        Character.Xpos = (uint)Position.X;
-        Character.Ypos = (uint)Position.Y;
-        Character.CurrentTarget = null;
-        Character.MapScale = 100;
-        Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
-        Character.RangedWeapons[0].Equipped = true;
-        Character.CurrentAimAmount = 20;
+	public override void _Ready()
+	{
+		Random rnd = new Random();
+		Character = new Character(7, 0);
+		Character.Name = rnd.Next(1, 10000).ToString();
+		Character.Xpos = (uint)Position.X;
+		Character.Ypos = (uint)Position.Y;
+		Character.CurrentTarget = null;
+		Character.MapScale = 100;
+		Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
+		Character.RangedWeapons[0].Equipped = true;
+		Character.CurrentAimAmount = 20;
 
-        _animatedSprite = GetNode<AnimatedSprite2D>("SoldierspriteAnimated");
+		_animatedSprite = GetNode<AnimatedSprite2D>("SoldierspriteAnimated");
 
-        //_animated_sprite.play("run");
-    }
+		//_animated_sprite.play("run");
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        //Running animation for Idle
-        _animatedSprite.Play("Idle");
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Running animation for Idle
+		_animatedSprite.Play("Idle");
+	}
 }
