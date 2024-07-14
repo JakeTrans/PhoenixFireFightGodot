b2ff3b9793334f34ecef1523695d3ad99de9a7b0
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-07-14 19:53:43 +0100

    Extra Animations setup

--- Units/Soldier.cs
+++ Units/Soldier.cs
@@ -5,65 +5,65 @@ using System;
 
 public partial class Soldier : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public Character Character { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public Character Character { get; set; }
 
-    private AnimatedSprite2D _animatedSprite;
+	private AnimatedSprite2D _animatedSprite;
 
-    private Animation ActiveAnimation;
+	private Animation ActiveAnimation;
 
-    private enum Animation
-    {
-        Idle,
-        Melee,
-        Move,
-        reload,
-        Shoot
-    }
+	private enum Animation
+	{
+		Idle,
+		Melee,
+		Move,
+		reload,
+		Shoot
+	}
 
-    public override void _Ready()
-    {
-        Random rnd = new Random();
-        Character = new Character(7, 0);
-        Character.Name = rnd.Next(1, 10000).ToString();
-        Character.Xpos = (uint)Position.X;
-        Character.Ypos = (uint)Position.Y;
-        Character.CurrentTarget = null;
-        Character.MapScale = 100;
-        Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
-        Character.RangedWeapons[0].Equipped = true;
-        Character.CurrentAimAmount = 20;
-        ActiveAnimation = Animation.Idle;
+	public override void _Ready()
+	{
+		Random rnd = new Random();
+		Character = new Character(7, 0);
+		Character.Name = rnd.Next(1, 10000).ToString();
+		Character.Xpos = (uint)Position.X;
+		Character.Ypos = (uint)Position.Y;
+		Character.CurrentTarget = null;
+		Character.MapScale = 100;
+		Character.RangedWeapons.Add(new RangedWeapon(1, WeaponType.AssaultRifles));
+		Character.RangedWeapons[0].Equipped = true;
+		Character.CurrentAimAmount = 20;
+		ActiveAnimation = Animation.Idle;
 
-        _animatedSprite = GetNode<AnimatedSprite2D>("SoldierspriteAnimated");
-    }
+		_animatedSprite = GetNode<AnimatedSprite2D>("SoldierspriteAnimated");
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        //Running animation for Idle
-        //_animatedSprite.Play("Idle");
-        switch (ActiveAnimation)
-        {
-            case Animation.Idle:
-                _animatedSprite.Play("Idle");
-                break;
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Running animation for Idle
+		//_animatedSprite.Play("Idle");
+		switch (ActiveAnimation)
+		{
+			case Animation.Idle:
+				_animatedSprite.Play("Idle");
+				break;
 
-            case Animation.Melee:
-                _animatedSprite.Play("Melee");
-                break;
+			case Animation.Melee:
+				_animatedSprite.Play("Melee");
+				break;
 
-            case Animation.Move:
-                _animatedSprite.Play("Move");
-                break;
+			case Animation.Move:
+				_animatedSprite.Play("Move");
+				break;
 
-            case Animation.reload:
-                _animatedSprite.Play("reload");
-                break;
+			case Animation.reload:
+				_animatedSprite.Play("reload");
+				break;
 
-            case Animation.Shoot:
-                _animatedSprite.Play("Shoot");
-                break;
-        }
-    }
+			case Animation.Shoot:
+				_animatedSprite.Play("Shoot");
+				break;
+		}
+	}
 }
--- main.cs
+++ main.cs
@@ -4,36 +4,36 @@ using System.Linq;
 
 public partial class main : Node
 {
-    private PackedScene Soldier = (PackedScene)GD.Load("res://Units/Soldier.tscn");
+	private PackedScene Soldier = (PackedScene)GD.Load("res://Units/Soldier.tscn");
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        for (uint i = 1; i < StoredData.NumberOfSoldiers + 1; i++)
-        {
-            Node CurrentSoldier = Soldier.Instantiate();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		for (uint i = 1; i < StoredData.NumberOfSoldiers + 1; i++)
+		{
+			Node CurrentSoldier = Soldier.Instantiate();
 
-            AddChild(CurrentSoldier);
+			AddChild(CurrentSoldier);
 
-            Soldier SoldierInstance = (Soldier)CurrentSoldier;
-            StoredData.Soldiers.Add(SoldierInstance);
-            SoldierInstance.Character.Xpos = (200 * i);
+			Soldier SoldierInstance = (Soldier)CurrentSoldier;
+			StoredData.Soldiers.Add(SoldierInstance);
+			SoldierInstance.Character.Xpos = (200 * i);
 
-            SoldierInstance.Character.Ypos = (200 * i);
-            SoldierInstance.GlobalPosition = new Vector2(SoldierInstance.Character.Xpos, SoldierInstance.Character.Ypos);
-            SoldierInstance.Character.Facing = FireFight.Functions.GeneralFunctions.CardinalDirectionsSquare.North;
-            SoldierInstance.Character.Sidereference = i;
-        }
+			SoldierInstance.Character.Ypos = (200 * i);
+			SoldierInstance.GlobalPosition = new Vector2(SoldierInstance.Character.Xpos, SoldierInstance.Character.Ypos);
+			SoldierInstance.Character.Facing = FireFight.Functions.GeneralFunctions.CardinalDirectionsSquare.North;
+			SoldierInstance.Character.Sidereference = i;
+		}
 
-        StoredData.Soldiers = StoredData.Soldiers.OrderByDescending(x => x.Character.INTSkillFactor).ToList();
-        StoredData.CurrentSoldierNode = StoredData.Soldiers[0];
-        StoredData.CurrentSoldierNode.Character.Selected = true;
+		StoredData.Soldiers = StoredData.Soldiers.OrderByDescending(x => x.Character.INTSkillFactor).ToList();
+		StoredData.CurrentSoldierNode = StoredData.Soldiers[0];
+		StoredData.CurrentSoldierNode.Character.Selected = true;
 
-        ((Sprite2D)StoredData.CurrentSoldierNode.FindChild("Selected")).Visible = true;
-    }
+		((Sprite2D)StoredData.CurrentSoldierNode.FindChild("Selected")).Visible = true;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
