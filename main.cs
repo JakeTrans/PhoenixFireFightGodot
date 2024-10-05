dcbdc4ddb7c5bd925f34b90084dcb2f432ef7605
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-10-05 17:02:18 +0100

    Scrolling working

--- main.cs
+++ main.cs
@@ -4,36 +4,64 @@ using System.Linq;
 
 public partial class main : Node
 {
-	private PackedScene Soldier = (PackedScene)GD.Load("res://Units/Soldier.tscn");
+    private PackedScene Soldier = (PackedScene)GD.Load("res://Units/Soldier.tscn");
+    private Camera2D camera;
+    private Vector2 cameraSpeed = new Vector2(500, 500);
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		for (uint i = 1; i < StoredData.NumberOfSoldiers + 1; i++)
-		{
-			Node CurrentSoldier = Soldier.Instantiate();
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        CreateSoliders();
+        camera = GetNode<Camera2D>("MainViewPoint");
+    }
 
-			AddChild(CurrentSoldier);
+    private void CreateSoliders()
+    {
+        for (uint i = 1; i < StoredData.NumberOfSoldiers + 1; i++)
+        {
+            Node CurrentSoldier = Soldier.Instantiate();
 
-			Soldier SoldierInstance = (Soldier)CurrentSoldier;
-			StoredData.Soldiers.Add(SoldierInstance);
-			SoldierInstance.Character.Xpos = (200 * i);
+            AddChild(CurrentSoldier);
 
-			SoldierInstance.Character.Ypos = (200 * i);
-			SoldierInstance.GlobalPosition = new Vector2(SoldierInstance.Character.Xpos, SoldierInstance.Character.Ypos);
-			SoldierInstance.Character.Facing = FireFight.Functions.GeneralFunctions.CardinalDirectionsSquare.North;
-			SoldierInstance.Character.Sidereference = i;
-		}
+            Soldier SoldierInstance = (Soldier)CurrentSoldier;
+            StoredData.Soldiers.Add(SoldierInstance);
+            SoldierInstance.Character.Xpos = (200 * i);
 
-		StoredData.Soldiers = StoredData.Soldiers.OrderByDescending(x => x.Character.INTSkillFactor).ToList();
-		StoredData.CurrentSoldierNode = StoredData.Soldiers[0];
-		StoredData.CurrentSoldierNode.Character.Selected = true;
+            SoldierInstance.Character.Ypos = (200 * i);
+            SoldierInstance.GlobalPosition = new Vector2(SoldierInstance.Character.Xpos, SoldierInstance.Character.Ypos);
+            SoldierInstance.Character.Facing = FireFight.Functions.GeneralFunctions.CardinalDirectionsSquare.North;
+            SoldierInstance.Character.Sidereference = i;
+        }
 
-		((Sprite2D)StoredData.CurrentSoldierNode.FindChild("Selected")).Visible = true;
-	}
+        StoredData.Soldiers = StoredData.Soldiers.OrderByDescending(x => x.Character.INTSkillFactor).ToList();
+        StoredData.CurrentSoldierNode = StoredData.Soldiers[0];
+        StoredData.CurrentSoldierNode.Character.Selected = true;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+        ((Sprite2D)StoredData.CurrentSoldierNode.FindChild("Selected")).Visible = true;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        Vector2 movement = new Vector2();
+
+        if (Input.IsActionPressed("ui_right"))
+        {
+            movement.X += 1;
+        }
+        if (Input.IsActionPressed("ui_left"))
+        {
+            movement.X -= 1;
+        }
+        if (Input.IsActionPressed("ui_down"))
+        {
+            movement.Y += 1;
+        }
+        if (Input.IsActionPressed("ui_up"))
+        {
+            movement.Y -= 1;
+        }
+
+        camera.Position += movement * cameraSpeed * new Vector2((float)delta, (float)delta);
+    }
 }
